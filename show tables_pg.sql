fb40fc784465fe73353754f5a53478a2b6693b0a
Nitin Sharma <157719378+NitinSharma004@users.noreply.github.com>
2025-06-08 23:53:05 +0530

    Add files via upload
    This is all tables create in SQL.

---
+++ show tables_pg.sql
@@ -0,0 +1,16 @@
+SELECT * FROM customers;
+SELECT COUNT(*) FROM customers;
+
+SELECT * FROM products;
+
+SELECT * FROM delivery_agents;
+
+SELECT * FROM orders;
+
+SELECT * FROM order_items;
+
+SELECT * FROM deliveries;
+
+SELECT * FROM delivery_status_log;
+
+SELECT * FROM returns;
